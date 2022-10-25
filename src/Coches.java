a2528b0db441093f62db3842ee2852645831bcba
AndreaENDES <andreavg37@educastur.es>
2022-10-25 09:39:36 +0200

    coche tercer commit

--- src/Coches.java
+++ src/Coches.java
@@ -3,4 +3,12 @@ public class Coches {
 
 	private String color;
 	private String marca;
+	
+	public Coches(String color, String marca) {
+		super();
+		this.color = color;
+		this.marca = marca;
+	}
+	
+	
 }
