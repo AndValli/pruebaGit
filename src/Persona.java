b3e6b32985155c424abd4ff4fe61505bff452739
AndreaENDES <andreavg37@educastur.es>
2022-10-25 09:37:31 +0200

    persona tercer commit

--- src/Persona.java
+++ src/Persona.java
@@ -3,4 +3,12 @@ public class Persona {
 
 	private String nombre;
 	private int edad;
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	
 }
