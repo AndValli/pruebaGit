a4b32393b5ec4d0c5f970c94c25614fda0e50031
AndreaENDES <andreavg37@educastur.es>
2022-10-25 10:19:46 +0200

    Merge branch 'coches'

--- src/Start.java
+++ src/Start.java
@@ -4,17 +4,13 @@ public class Start {
 	public static void main(String[] args) {
 		
 		
-<<<<<<< HEAD
+
 		System.out.println("hola mundo");
-
 		System.out.println("master hola");
-
 		System.out.println("persona hola");
-
-=======
 		System.out.println("hol mundo");
 		System.out.println("coche hola");
->>>>>>> refs/heads/coches
+
 	}
 
 }
