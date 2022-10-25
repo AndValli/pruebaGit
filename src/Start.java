ced573b78c790625556f381deef6cc8dd11ac118
AndreaENDES <andreavg37@educastur.es>
2022-10-25 09:53:22 +0200

    avance master

--- src/Start.java
+++ src/Start.java
@@ -4,8 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("hol mundo");
-
+		System.out.println("hola mundo");
+		System.out.println("master hola");
 	}
 
 }
